83d9f56e98417ebd5505018f10cba37d14b58631
sabareesanrajkumar <145452371+sabareesanrajkumar@users.noreply.github.com>
2024-11-18 23:48:10 +0530

    Add files via upload
    server responds with html

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const http = require('http');
 
 const server = http.createServer((req, res)=>{
     console.log('hi sabareesan');
-})
+    const {url} = req;
+    res.setHeader( 'Content-Type','text/html');
+    if (url == '/home') {
+        res.write(`<html><body>Welcome Home</body></html>`);
+        res.end();
+    } else if (url == '/about') {
+        res.write(`<html><body>Welcome to About Us page</body></html>`);
+        res.end();
+    } else if (url == '/node') {
+        res.end(`<html><body>Welcome to my Node Js project</body></html>`);
+    } else {
+        res.end('404 Not Found');
+    }
+});
 
 server.listen(4000);
